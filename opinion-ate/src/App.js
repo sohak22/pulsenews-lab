e92a8406df83e551d3d38e180ba7c02dd4f04511
Swetaa Suresh <swetaa232002@gmail.com>
2023-03-08 12:55:10 -0500

    Lab 8 initialization

--- opinion-ate/src/App.js
+++ opinion-ate/src/App.js
@@ -1,5 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { incrementByAmount } from './features/counter/counterSlice';
+import React, { Component, useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  <TextField
+    id="standard-basic"
+    label="Standard"
+    variant="standard"
+    value = {counter}
+    onChange= {(e) => setCounter(e.target.value)}
+  />
+
+}
 
 function App() {
   return (
@@ -9,6 +26,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Counter></Counter>
         <a
           className="App-link"
           href="https://reactjs.org"
