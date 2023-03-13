bc6a94207328bc23b491f71255cfa9098a0ee985
sohak22 <sohakn22@gmail.com>
2023-03-13 14:26:22 -0400

    Add restaurantscreen and restaurantlist

--- opinion-ate/src/App.js
+++ opinion-ate/src/App.js
@@ -23,7 +23,12 @@
 // }
 
 // export default App;
+import RestaurantScreen from './components/RestaurantScreen';
 
 export default function App() {
-  return <div>Hello, world.</div>;
+  return (
+    <div>
+      <RestaurantScreen />
+    </div>
+  );
 }
---
+++ opinion-ate/src/components/RestaurantScreen.js
@@ -0,0 +1,6 @@
+import RestaurantList from './RestaurantList';
+export default function RestaurantScreen() {
+    return (
+        <div> <h1>Restaurants</h1> <RestaurantList />
+        </div>);
+}
